9cdd4383e40fe5c1483a2659ad6bbcfd1d602c0a
ravsa0109 <rk5177582@gmail.com>
2025-10-01 15:05:04 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,6 +7,3 @@ def keep_alive():
     app = web.Application()
     app.add_routes([web.get("/", handle)])
     web.run_app(app, host="0.0.0.0", port=8080)
-
-if __name__ == "__main__":
-    keep_alive()
